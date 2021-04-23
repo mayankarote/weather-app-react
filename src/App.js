09d276213bb13475b2fa1a8a023fe5c032e92594
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-04-24 00:16:34 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends React.Component{
     const country = e.target.elements.country.value;
 
     if (city && country) {
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=f2858a9fd55f54e97f678fd46dcfcb13`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=f2858a9fd55f54e97f678fd46dcfcb13`);
 
       const response = await api_call.json();
       console.log(response);
